9fba7e4040ca25eac5777141c4dbbcb18ee87f2b
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-04-26 21:49:49 +0700

    update config_reader

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
 import discord
 
-from config_reader import init
+from config_reader import ConfigReader
 
-TOKEN = init()
+TOKEN = ConfigReader.get_config("Tokens", "bot-token")
+
+class LocalizationManagementBot(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = LocalizationManagementBot()
+client.run(TOKEN)
