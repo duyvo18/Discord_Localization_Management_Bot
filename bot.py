c17dde4d8ac36a6bb8c7c5bf2ffcc7f0f68892ef
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-05-02 17:51:22 +0700

    sync update

--- bot.py
+++ bot.py
@@ -5,10 +5,20 @@ from config_reader import ConfigReader
 TOKEN = ConfigReader.get_config("Tokens", "bot-token")
 
 class LocalizationManagementBot(discord.Client):
+    def __init__(self, TOKEN):
+        intents = discord.Intents.all()
+        discord.Client(intents=intents)
+        super().__init__()
+        self.run(TOKEN)
+
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        if message.author.id != self.user.id:
+            channel = self.get_channel(968450197581598723)
+            await channel.send(message.content)
+        
         print('Message from {0.author}: {0.content}'.format(message))
         
 
@@ -16,6 +26,5 @@ class LocalizationManagementBot(discord.Client):
         # TODO: u sure this is a good idea
         pass
         
-
-client = LocalizationManagementBot()
-client.run(TOKEN)
+if __name__ == '__main__':
+    LocalizationManagementBot(TOKEN)
